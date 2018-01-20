5b2bac5ecc1a9382b4a5079386521dae4d74748c
deliacodes <deliacodes@gmail.com>
2018-01-19 19:12:14 -0800

    Adds get, put, delete, and post recipe tests.

--- test/test-server.js
+++ test/test-server.js
@@ -171,10 +171,65 @@ describe('Recipes', function () {
         expect(res.body).to.be.a('array');
         expect(res.body.length).to.be.least(1);
         const expectedRecipeKeys = ['id', 'name'];
-        res.body.forEach(function (item) {
-          expect(item).to.be.a('object');
-          expect(item).to.include.keys(expectedRecipeKeys);
+        res.body.forEach(function (recipe) {
+          expect(recipe).to.be.a('object');
+          expect(recipe).to.include.keys(expectedRecipeKeys);
         });
+      });
+  });
+
+  it('should add a recipe on POST', function () {
+    const newRecipe = {
+      name: 'Cafe Au Lait',
+      ingredients: ['coffee', 'chicory', 'milk']
+    };
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function (res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newRecipe, {
+          id: res.body.id
+        }));
+      });
+  });
+
+  it('should update items on PUT', function () {
+
+    const updateRecipeData = {
+      name: 'tea au lait',
+      ingredients: ['tea', 'milk']
+    };
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        updateRecipeData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/shopping-list/${updateRecipeData.id}`)
+          .send(updateRecipeData);
+      })
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateRecipeData);
+      });
+  });
+
+  it('should delete items on DELETE', function () {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function (res) {
+        expect(res).to.have.status(204);
       });
   });
 
