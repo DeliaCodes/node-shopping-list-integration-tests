a1158a5fcd7da7b9ea9f8f115b9da3e16776e411
deliacodes <deliacodes@gmail.com>
2018-02-01 22:11:43 -0800

    Update unit test roughed out.

--- test/test-models.js
+++ test/test-models.js
@@ -17,6 +17,18 @@ describe('Models', function () {
       expect(items).to.include.keys(newItem.id);
     });
 
+    it('successfully updates a recipe', () => {
+      const createRecipe = Recipes.create();
+      const getNewRecipe = Recipes.get();
+      const newRecipeName = {
+        id: getNewRecipe.id,
+        name: 'Milk'
+      };
+      const updatedRecipe = Recipes.update(newRecipeName);
+      const getUpdatedRecipe = Recipes.get();
+      expect(getUpdatedRecipe.name).to.be('Milk');
+    });
+
     //Recipes.create('Milk', );
     //What is your expectations?
     // expect(res of Recipes.get()) to.contain {'boiled white rice', 'milkshake'}
